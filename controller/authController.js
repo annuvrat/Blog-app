b7f3de712cc9dbc9f0e1251ad661aeb1d7ed29f6
Annuvrat <115866889+annuvrat@users.noreply.github.com>
2025-02-04 12:28:27 +0530

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -21,14 +21,14 @@ export const authController = {
 
             res.cookie('access_token', accessToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
+                secure: process.env.NODE_ENV,
                 maxAge: 15 * 60 * 1000 // 15 minutes
             });
 
 
             res.cookie('refresh_token', refreshToken, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
+                secure: process.env.NODE_ENV,
                 maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
             });
 
